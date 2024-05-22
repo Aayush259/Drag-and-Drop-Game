3d8d2f96e59964e04faf6b3c31fa6df7011eacf0
Square <aayushk4980@gmail.com>
2024-05-23 01:16:41 +0530

    Added options hL2 to vrLS4

---
+++ scripts/App.js
@@ -0,0 +1,1 @@
+import getOption from "./Options.js";
---
+++ scripts/Options.js
@@ -0,0 +1,195 @@
+const vL2 = `
+    <div class="option" id="vL2">
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const vL3 = `
+    <div class="option" id="vL3">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const vL4 = `
+    <div class="option" id="vL4">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const vL5 = `
+    <div class="option" id="vL5">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const vL6 = `
+    <div class="option" id="vL6">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const hL2 = `
+    <div class="option flex" id="hL2">
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const hL3 = `
+    <div class="option flex" id="hL3">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const hL4 = `
+    <div class="option flex" id="hL4">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+
+
+const hL5 = `
+    <div class="option flex" id="hL5">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const hL6 = `
+    <div class="option flex" id="hL6">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const d = `
+    <div class="option" id="d">
+        <div class="box"></div>
+    </div>
+`;
+
+const hLS3 = `
+    <div class="option" id="hLS3">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const hLS4 = `
+    <div class="option" id="hLS4">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const hrLS3 = `
+    <div class="option" id="hrLS3">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const hrLS4 = `
+    <div class="option" id="hrLS4">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const vLS3 = `
+    <div class="option" id="vLS3">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const vLS4 = `
+    <div class="option" id="vLS4">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const vrLS3 = `
+    <div class="option" id="vrLS3">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+const vrLS4 = `
+    <div class="option" id="vrLS4">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+`;
+
+
+const options = {
+    hL2: hL2,
+    hL3: hL3,
+    hL4: hL4,
+    hL5: hL5,
+    hL6: hL6,
+    vL2: vL2,
+    vL3: vL3,
+    vL4: vL4,
+    vL5: vL5,
+    vL6: vL6,
+    d:d,
+    hLS3: hLS3,
+    hLS4: hLS4,
+    hrLS3: hrLS3,
+    hrLS4: hrLS4,
+    vLS3: vLS3,
+    vLS4: vLS4,
+    vrLS3: vrLS3,
+    vrLS4: vrLS4
+}
+
+function getOption(name) {
+    return options[name];
+}
+
+export default getOption;
